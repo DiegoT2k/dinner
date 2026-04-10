5c9de0d3cec92d572296a2933b3f2e75a632e6aa
Diego Tarantola <d.tarantola@reply.it>
2026-04-10 09:57:27 +0200

    Initial Commit

---
+++ src/main/java/it/project/dinner/DinnerApplication.java
@@ -0,0 +1,13 @@
+package it.project.dinner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinnerApplication.class, args);
+	}
+
+}
